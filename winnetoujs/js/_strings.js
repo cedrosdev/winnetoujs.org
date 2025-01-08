3a049a0fa27d8b4b40bd99cf4e30c6077171bb69
Pamela Ubuntu <kaue.sedrez@gmail.com>
2025-01-07 22:08:47 -0300

    updated change log

---
+++ winnetoujs/js/_strings.js
@@ -0,0 +1,13 @@
+import {
+  Winnetou
+} from "../node_modules/winnetoujs/src/winnetou.js";
+
+export default Winnetou.strings = {
+
+  /** @property hello 2 */
+  title: "hello 2",
+
+  /** @property worked */
+  text: "worked",
+
+}
